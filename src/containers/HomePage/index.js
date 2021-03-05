63a049165b569ab6843956219eb1ab9db1c93c87
AnnaKubackova <60429066+AnnaKubackova@users.noreply.github.com>
2021-03-05 12:36:26 +0100

    users container flex

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -12,7 +12,7 @@ const User = (props) => {
   return (
     <div onClick={() => getUserToChat(user)} className="user">
         <img src="https://i.pinimg.com/originals/be/ac/96/beac96b8e13d2198fd4bb1d5ef56cdcf.jpg" alt="" />
-        <p>{user.firstName} <br /> {user.lastName}</p>
+        <p>{user.firstName} {user.lastName}</p>
     </div>
   )
 }
